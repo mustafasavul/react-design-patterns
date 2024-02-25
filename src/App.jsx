9e5638129c4ce4bed27f6a95de4cdef0960b0ba8
EXT02D53265 <mustafasavul4434@hotmail.com>
2024-02-25 21:06:23 +0300

    Remove SplitScreen component and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,11 @@
 import './App.css'
-import { SplitScreen } from './components/SplitScreen';
-
-const LeftSideComp = () => {
-  return (
-    <div className="left-side">
-      <h1 style={{
-        color: 'white',
-        backgroundColor: 'black'
-      }}>Left Side</h1>
-    </div>
-  )
-}
-
-const RightSideComp = () => {
-  return (
-    <div className="right-side">
-      <h1 style={{
-        color: 'black',
-        backgroundColor: 'white'
-      
-      }}>Right Side</h1>
-    </div>
-  )
-}
 
 function App() {
   return (
     <>
-      <SplitScreen 
-       leftWidth={6}
-       rightWidth={6}
-      >
-        <LeftSideComp title={'Left Side'}/>
-        <RightSideComp title={'Right Side'} />
-      </SplitScreen>
+    <div>
+      
+    </div>
     </>
   )
 }
